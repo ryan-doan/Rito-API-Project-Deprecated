fa03bfe166dd8a5366de57c422d885a4fd5a10a5
ryan-doan <82603138+ryan-doan@users.noreply.github.com>
2021-08-28 17:02:58 -0400

    Update Launcher.java

--- Rito API Project/src/Launcher.java
+++ Rito API Project/src/Launcher.java
@@ -77,7 +77,10 @@ public class Launcher implements ActionListener {
                     } catch (Exception exception) {
                         exception.printStackTrace();
                     }
-
+                    
+                    //set player variable's rank data. See rankData method for more information
+                    
+                    rankedData(player);
                 } else {
                     System.out.println("You typed " + player.getName());
                     startingGUI.getErrorLabel().setText("Incorrect username.. Try again!");
@@ -108,7 +111,9 @@ public class Launcher implements ActionListener {
             // Formats data
 
             rawData = rawData.substring(1, rawData.length() - 1).replaceAll("\"",
-                    "");
+                    "").replaceAll("[a-zA-Z]*:", "");
+
+            System.out.println(rawData);
 
             String[] summonerData = rawData.split(",");
 
@@ -121,20 +126,74 @@ public class Launcher implements ActionListener {
     public Summoner newSummoner(String[] summonerData) {
         //formats data from the string[] and use it to create a new player object
 
-        Summoner player = new Summoner(summonerData[0].replaceAll("id:", ""),
-                summonerData[1].replaceAll("adccountId", ""),
-                summonerData[2].replaceAll("puuid:", ""),
-                summonerData[3].replaceAll("name:", ""),
-                Integer.parseInt(summonerData[4].replaceAll("profileIconId:", "")),
-                summonerData[5].replaceAll("revisionDate:", ""),
-                Integer.parseInt(summonerData[6].replaceAll("summonerLevel:", "")));
+        Summoner player = new Summoner(summonerData[0], summonerData[1], summonerData[2], summonerData[3],
+                Integer.parseInt(summonerData[4]), summonerData[5], Integer.parseInt(summonerData[6]));
 
         return player;
     }
 
+    public void rankedData(Summoner player) throws Exception {
+        //Connects to Rito's API
+
+        URL url = new URL("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"
+                + player.getId() + API_key);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String rawData = reader.readLine();
+
+        if (rawData.length() < 5) {  //player doesn't play ranked
+            
+            player.setSoloQ(null);
+            player.setFlex(null);
+            
+        } else {
+            
+            rawData = rawData.substring(1, rawData.length() - 1).replaceAll("[\"{}]",
+                    "").replaceAll("[a-zA-Z]*:", "");
+
+            String[] rankedData = rawData.split(",");
+
+            if (rankedData.length < 14) {  //only 1 rank queue
+                
+                if (rankedData[1].equals("RANKED_SOLO_5x5")) {
+                    //  see rankQueue method in this class
+
+                    player.setSoloQ(rankQueue(rankedData));
+                } else {
+                    player.setFlex(rankQueue(rankedData));
+                }
+                
+            } else {  //both rank queue
+                
+                player.setSoloQ(rankQueue(rankedData));
+                player.setFlex(new RankQueue(rankedData[14], rankedData[15], Integer.parseInt(rankedData[16]),
+                        Integer.parseInt(rankedData[19]), Integer.parseInt(rankedData[20]),
+                        Integer.parseInt(rankedData[21]), Boolean.parseBoolean(rankedData[22]),
+                        Boolean.parseBoolean(rankedData[23]), Boolean.parseBoolean(rankedData[24]),
+                        Boolean.parseBoolean(rankedData[25])));
+                
+            }
+            
+        }
+
+    }
+
+    public RankQueue rankQueue(String[] rankedData) {
+        
+        return new RankQueue(rankedData[1], rankedData[2], Integer.parseInt(rankedData[3]),
+                Integer.parseInt(rankedData[6]), Integer.parseInt(rankedData[7]),
+                Integer.parseInt(rankedData[8]), Boolean.parseBoolean(rankedData[9]),
+                Boolean.parseBoolean(rankedData[10]), Boolean.parseBoolean(rankedData[11]),
+                Boolean.parseBoolean(rankedData[12]));
+        
+    }  //create a rankQueue object from the data in the String array
+
     public static void skipLine(BufferedReader reader, int lines) throws Exception {
+        
         for (int i = 0; i < lines; i++) {
             reader.readLine();
         }
+        
     }  //this is to skip multiple lines on a txt file
 }
